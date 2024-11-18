a300168d910c76a0f8e0d94b98e6f4a8473060b5
sneha31415 <sneha.singh.31415@gmail.com>
2024-11-18 14:26:42 +0530

    completed login page and added the layout for billing page

--- main.py
+++ main.py
@@ -23,14 +23,72 @@ class LoginPage:
 
         self.entry_frame = LabelFrame(self.main_frame, text = "Enter Details", bd = 6, relief=GROOVE, bg = "lightgrey", font = ("sans-serif", 18))
         self.entry_frame.pack(fill = BOTH, expand=TRUE)
+        
+        # ---------variables for username and pwd--------
+        username = StringVar()
+        password = StringVar()
 
-
+        # ----------username----------
         self.user_label = Label(self.entry_frame, text = "Enter username : ", bg = "lightgrey", font = ("sans-serif", 15))
         self.user_label.grid(row = 0, column = 0, padx = 2, pady = 2)
 
-        # ----------variables----------
-        self.user_entry = Entry(self.entry_frame, font = ("sans-serif", 15))
+        self.user_entry = Entry(self.entry_frame, font = ("sans-serif", 15), bd = 3, textvariable=username)
         self.user_entry.grid(row = 0, column=1, padx=2,pady = 2)
+
+        # ---------password----------
+        self.password_label = Label(self.entry_frame, text = "Enter password : ", bg = "lightgrey", font = ("sans-serif", 15))
+        self.password_label.grid(row = 1, column = 0, padx = 2, pady = 2)
+
+        self.password_entry = Entry(self.entry_frame, font = ("sans-serif", 15), bd = 3, textvariable=password, show = '*')
+        self.password_entry.grid(row = 1, column=1, padx=2,pady = 2)
+
+        # --------functions ------------
+        def check_login():
+            '''
+            this will check 
+            '''
+            if username.get() == "sneha" and password.get() == "1234":
+                self.billing_btn.config(state = "normal")
+            else:
+                pass #-> message box
+        
+        def reset():
+            '''
+            clears the username and password
+            '''
+            username.set("")
+            password.set("")
+
+        def billing_sect():
+            '''
+            open a billing window on login
+            '''
+            self.new_window = Toplevel(self.root)
+            self.app = Window2(self.new_window)
+    
+
+        # ---------buttons----------
+        self.button_frame = LabelFrame(self.entry_frame, text = "Options", font = ("Arial", 15), bg = "lightgrey", bd = 7, relief = GROOVE)
+        self.button_frame.place(x = 20, y = 100, width = 730, height = 90)
+        
+        self.login_btn = Button(self.button_frame, text = "Login", font = ("Arial", 15), bd = 5, width = 15, command=check_login)
+        self.login_btn.grid(row = 0, column= 0, padx =20, pady=2)
+
+        self.billing_btn = Button(self.button_frame, text = "Billing", font = ("Arial", 15), bd = 5, width = 15, pady=2, command = billing_sect)
+        self.billing_btn.grid(row = 0, column= 1, padx =20)
+        # bydefault billing is disabled
+        self.billing_btn.config(state = "disabled")
+        
+        self.reset_btn = Button(self.button_frame, text = "Reset", font = ("Arial", 15), bd = 5, width = 15, command=reset)
+        self.reset_btn.grid(row = 0, column= 2, padx =20, pady=2)
+
+
+# --------billing window-----------
+class Window2:
+    def __init__(self, root):
+        self.root = root
+        self.root.geometry("1350x750+0+0")
+        self.root.title("Billing System")
 
 if __name__ == "__main__":
     main()
