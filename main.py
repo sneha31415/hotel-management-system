810b78bc50aadbea7bbbe8706e1f739cd80303a8
sneha31415 <sneha.singh.31415@gmail.com>
2024-11-18 11:47:47 +0530

    login page half done

--- main.py
+++ main.py
@@ -15,7 +15,22 @@ class LoginPage:
         self.title_label = Label(self.root, text = "Hotel Management System", font = ('Arial', 35, 'bold'), bg = "lightgrey", bd = 8, relief = GROOVE)
         self.title_label.pack(side = TOP, fill = 'x')
 
+        self.main_frame = Frame(self.root, bg = "lightgrey", bd = 6, relief=GROOVE)
+        self.main_frame.place(x = 280, y = 150, width = 800, height = 450)
 
+        self.login_label = Label(self.main_frame, text = "Login", bd = 6, relief=GROOVE, anchor = CENTER, bg = "lightgrey", font = ("sans-serif", 25, "bold"))
+        self.login_label.pack(side = TOP, fill = "x")
+
+        self.entry_frame = LabelFrame(self.main_frame, text = "Enter Details", bd = 6, relief=GROOVE, bg = "lightgrey", font = ("sans-serif", 18))
+        self.entry_frame.pack(fill = BOTH, expand=TRUE)
+
+
+        self.user_label = Label(self.entry_frame, text = "Enter username : ", bg = "lightgrey", font = ("sans-serif", 15))
+        self.user_label.grid(row = 0, column = 0, padx = 2, pady = 2)
+
+        # ----------variables----------
+        self.user_entry = Entry(self.entry_frame, font = ("sans-serif", 15))
+        self.user_entry.grid(row = 0, column=1, padx=2,pady = 2)
 
 if __name__ == "__main__":
     main()
